c43e60cf23893f1ea13fc03ca4dffb44ce124020
Delgorithm <delgorithm@gmail.com>
2023-11-01 18:56:11 +0100

    fix: naivgation in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Accessories from './Pages/Product/Accessories';
-import Jacket from './Pages/Product/jacket';
-import Tshirts from './Pages/Product/tshirts';
+import Tshirts from './Pages/Product/Tshirts';
 import Cart from './Pages/Cart/Cart';
+import Jacket from './Pages/Product/jacket';
 
 function App() {  
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route>
           <Route path='/' element={<Home />} />
           <Route path='/accessories' element={<Accessories />} />
-          <Route path='/jacket' element={< Jacket/>} />
+          <Route path='/jacket' element={<Jacket />} />
           <Route path='/tshirts' element={< Tshirts/>} />
           <Route path='/cart' element={<Cart />} />
         </Route>
--- src/Pages/Product/Jacket.jsx
+++ src/Pages/Product/Jacket.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from '../../Layout/Header/Header'
 import Footer from '../../Layout/Footer/Footer'
-import { useNavigate } from 'react-router-dom'
 import { jacketPage } from '../../data/DataText'
 
 const Jacket = () => {
