269d3223128dbae48b677760aba43388f667a30b
Delgorithm <delgorithm@gmail.com>
2023-12-06 15:08:52 +0100

    feature : carroussel for a product (chemise)

---
+++ src/Components/CarrousselProduct.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+
+export default function CarrousselProduct({ children: slides }) {
+
+    const [curr, setCurr] = useState(0);
+
+    const prev = () => 
+        setCurr((curr => curr === 0 ? slides.length - 1 : curr - 1));
+    const next = () => 
+        setCurr((curr => curr === slides.length - 1 ? 0 : curr + 1));
+
+    return (
+        <div className="overflow-hidden relative">
+            <div 
+                className="flex transition-transform ease-in-out duration-300" 
+                style={{transform: `translateX(-${curr * 100}%)`}}>{slides}
+            </div>
+            <div className="absolute inset-0 flex items-center justify-between p-1">
+                <button
+                    onClick={prev}
+                    className="p-0.5 rounded-full bg-lightgray hover:bg-white"
+                >
+                    <MdChevronLeft size={35}/>
+                </button>
+                <button 
+                    onClick={next}
+                    className="p-0.5 rounded-full bg-lightgray hover:bg-white"
+                >
+                    <MdChevronRight size={35}/>
+                </button>
+            </div>
+
+            <div className="absolute bottom-4 right-0 left-0">
+                <div className="flex items-center justify-center gap-2">
+                    {slides.map((_, i) => (
+                        <div className={`
+                            transition-all w-3 h-3 bg-white rounded-full
+                            ${curr === i ? "p-2" : "bg-opacity-50"}
+                        `} />
+                    ))}
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
--- src/Pages/Chemise.jsx
+++ src/Pages/Chemise.jsx
@@ -2,12 +2,32 @@ import React from 'react'
 
 import Footer from '../Components/Footer'
 import Navbar from '../Components/Navbar'
+import CarrousselProduct from '../Components/CarrousselProduct'
+
+const slides = [
+  "/Images/men/chemise/chemise01.jpeg",
+  "/Images/men/chemise/chemise02.jpeg",
+  "/Images/men/chemise/chemise03.jpeg",
+  "/Images/men/chemise/chemise04.jpeg",
+  "/Images/men/chemise/chemise05.jpeg",
+  "/Images/men/chemise/chemise06.jpeg",
+  "/Images/men/chemise/chemise07.jpeg",
+  "/Images/men/chemise/chemise08.jpeg",
+]
+
 
 const Chemise = () => {
   return (
     <div>
       <Navbar />
-      <p className='mt-16'>Chemise section</p>
+      <div className='mt-16'></div>
+      <section className='max-w-lg'>
+        <CarrousselProduct>
+          {slides.map((item, index) => (
+            <img key={index} src={item} alt="" />
+          ))}
+        </CarrousselProduct>
+      </section>
       <Footer />
     </div>
   )
